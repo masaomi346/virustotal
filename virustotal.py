a8ba59e76c6c24d6af81b545c20cc63f5df35af6
masaomi <masaomi346@gmail.com>
2020-02-28 15:10:34 +0900

    Python

--- virustotal.py
+++ virustotal.py
@@ -11,14 +11,18 @@ import os
 import time
 import subprocess
 import shlex
+import schedule
 
 def scan():
     files = []
+    analysis = []
     path = "/opt/dionaea/var/lib/dionaea/binaries/"
     for f in os.listdir(path):
         if os.path.isfile(os.path.join(path,f)) : files.append(f)
-    if len(files) == 0 : return "No Malware"
-    output = "Numbers:{}\n\n".format(len(files))
+    if len(files) == 0 :
+        analysis.append("No Malware")
+        return analysis
+    analysis.append("Numbers:{}\n\n".format(len(files)))
     for hash in files:
         url = "https://www.virustotal.com/vtapi/v2/file/report"
         params = {"resource": hash, "apikey": "APIKEY"}
@@ -29,13 +33,16 @@ def scan():
         if len(res) != 0 and res[-1].get("response_code") == 1 :
             cmd = "file " + path + "{}".format(hash)
             result = str(subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE).communicate()[0])
-            output += "Type:{0}\nHash:{1}\nTotal:{2}\nDetect:{3}\nURL:{4}\n\n".format(result.replace("/opt/dionaea/var/lib/dionaea/binaries/{}".format(hash),""),hash,res[-1].get("total"),res[-1].get("positives"),res[-1].get("permalink"))
-    return output
+            analysis.append("Type:{0}\nHash:{1}\nTotal:{2}\nDetect:{3}\nURL:{4}\n\n".format(result.replace("/opt/dionaea/var/lib/dionaea/binaries/{}".format(hash),""),hash,res[-1].get("total"),res[-1].get("positives"),res[-1].get("permalink")))
+    return send(analysis)
 
-def send(result):
-    cmd = "curl -X POST -H 'Content-type: application/json' --data {} https://hooks.slack.com/services/TOKEN"
-    cmd = cmd.format(json.dumps('{\"text\":\"' + result + '\"}'))
-    subprocess.call(shlex.split(cmd))
+def send(analysis):
+    for i in range(len(analysis)):
+        cmd = "curl -X POST -H 'Content-type: application/json' --data {} https://hooks.slack.com/services/TOKEN"
+        cmd = cmd.format(json.dumps('{\"text\":\"' + analysis[i] + '\"}'))
+        subprocess.call(shlex.split(cmd))
 
-result = scan()
-send(result)
+schedule.every().day.at("20:00").do(scan)
+while(True):
+    schedule.run_pending()
+    time.sleep(60)
