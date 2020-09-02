1232c53195a5341ab866555a95faabac796eb46a
masaomi <masaomi346@gmail.com>
2020-09-02 13:02:45 +0900

    Python

--- virustotal.py
+++ virustotal.py
@@ -12,17 +12,20 @@ import time
 import subprocess
 import shlex
 
-def scan():
+def analyze():
     files = []
-    analysis = []
+    result = ""
     try:
         path = "/opt/dionaea/var/lib/dionaea/binaries/"
         for f in os.listdir(path):
             if os.path.isfile(os.path.join(path,f)) : files.append(f)
         if len(files) == 0 :
-            analysis.append("No Malware")
-            return analysis
-        analysis.append("Numbers:{}\n\n".format(len(files)))
+            result = "No Malware"
+            send(result)
+            return
+        else:
+            result = "Numbers:{}\n\n".format(len(files))
+            send(result)
         for hash in files:
             url = "https://www.virustotal.com/vtapi/v2/file/report"
             params = {"resource": hash, "apikey": "APIKEY"}
@@ -31,17 +34,18 @@ def scan():
             res = urllib.request.urlopen(req).read().decode("utf-8")
             res = [json.loads(res) for s in res if s != ""]
             cmd = "file " + path + "{}".format(hash)
-            result = str(subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE).communicate()[0])
-            analysis.append("Type:{0}\nHash:{1}\nTotal:{2}\nDetect:{3}\nURL:{4}\n\n".format(result.replace("/opt/dionaea/var/lib/dionaea/binaries/{}".format(hash),""),hash,res[-1].get("total"),res[-1].get("positives"),res[-1].get("permalink")))
+            cmd = str(subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE).communicate()[0])
+            result = "Type:{0}\nHash:{1}\nTotal:{2}\nDetect:{3}\nURL:{4}\n\n".format(cmd.replace(path + "{}".format(hash),""),hash,res[-1].get("total"),res[-1].get("positives"),res[-1].get("permalink"))
+            send(result)
+            time.sleep(1)
     except Exception as e:
-        analysis.append(e)
-    return send(analysis)
+        send(e)
 
-def send(analysis):
-    for i in range(len(analysis)):
-        cmd = "curl -X POST -H 'Content-type: application/json' --data {} https://hooks.slack.com/services/TOKEN"
-        cmd = cmd.format(json.dumps('{\"text\":\"' + analysis[i] + '\"}'))
-        subprocess.call(shlex.split(cmd))
+def send(result):
+    push = "curl -X POST -H 'Content-type: application/json' --data {} https://hooks.slack.com/services/TOKEN"
+    if type(result) is str:
+        push = push.format(json.dumps('{\"text\":\"' + result + '\"}'))
+        subprocess.call(shlex.split(push))
 
-result = scan()
+result = analyze()
 send(result)
