edb8eeb843021e6583731a91daef4b20b54d3e7b
masaomi <masaomi346@gmail.com>
2020-10-11 14:51:15 +0900

    Python

--- virustotal.py
+++ virustotal.py
@@ -11,31 +11,29 @@ import os
 import time
 import subprocess
 import shlex
-import traceback
 
 def analyze():
     files = []
-    try:
-        path = "/opt/dionaea/var/lib/dionaea/binaries/"
-        for f in os.listdir(path):
-            if os.path.isfile(os.path.join(path,f)) : files.append(str(f))
-        if len(files) == 0 :
-            result.append("No Malware")
-            return result
-        result.append("Numbers:{}\n\n".format(len(files)))
-        for hash in files:
-            url = "https://www.virustotal.com/vtapi/v2/file/report"
-            params = {"resource": hash, "apikey": "APIKEY"}
-            post = urllib.parse.urlencode(params)
-            req = urllib.request.Request(url,post.encode("ascii"))
-            res = urllib.request.urlopen(req).read().decode("utf-8")
-            res = [json.loads(res) for s in res if s != ""]
+    path = "/opt/dionaea/var/lib/dionaea/binaries/"
+    for f in os.listdir(path):
+        if os.path.isfile(os.path.join(path,f)) : files.append(str(f))
+    if len(files) == 0 :
+        result.append("No Malware")
+        return result
+    result.append("Numbers:{}\n\n".format(len(files)))
+    for hash in files:
+        url = "https://www.virustotal.com/api/v3/files/" + hash
+        key = {"x-apikey": "APIKey"}
+        req = urllib.request.Request(url,headers=key)
+        try:
             cmd = "file " + path + "{}".format(hash)
             cmd = str(subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE).communicate()[0])
-            result.append("Type:{0}\nHash:{1}\nTotal:{2}\nDetect:{3}\nURL:{4}\n\n".format(cmd.replace("/opt/dionaea/var/lib/dionaea/binaries/{}".format(hash),""),hash,res[-1].get("total"),res[-1].get("positives"),res[-1].get("permalink")))
-    except:
-        result.append(traceback.print_exc())
-    return result
+            with urllib.request.urlopen(req) as res:
+                res = json.loads(res.read().decode("utf-8"))
+                result.append("Type:{0}\nHash:{1}\nMalicious:{2}\nSuspicious:{3}\nUndetected:{4}\nURL: https://www.virustotal.com/gui/file/{5}\n\n".format(cmd.replace(path + "{}".format(hash),""),hash,res["data"]["attributes"]["last_analysis_stats"].get("malicious"),res["data"]["attributes"]["last_analysis_stats"].get("suspicious"),res["data"]["attributes"]["last_analysis_stats"].get("undetected"),res["data"].get("id")))
+        except:
+            result.append("Message:{}".format(sys.exc_info()))
+            pass
 
 def send(result):
     for data in result:
